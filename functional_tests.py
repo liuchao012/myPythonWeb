07dd8b39935252de595af5e329160fe124a5d50a
liuch <liuchao012@126.com>
2018-06-25 21:04:23 +0800

    创建django项目成功

---
+++ functional_tests.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/25 20:15
+# @Author  : Mat
+# @Email   : mat_wu@163.com
+# @File    : functional_tests.py
+# @Software: PyCharm
+
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+driver.get("http://localhost:8000")
+assert 'Django' in driver.title
