c3a0ec9a1ce9858f4af679d0e2e2194a3e7c793d
liuch <liuchao012@126.com>
2018-06-26 16:04:41 +0800

    测试第一环节

--- functional_tests.py
+++ functional_tests.py
@@ -2,7 +2,6 @@
 # @Time    : 2018/6/25 20:15
 # @Author  : Mat
 # @Email   : mat_wu@163.com
-# @File    : functional_tests.py
 # @Software: PyCharm
 
 from selenium import webdriver
@@ -10,3 +9,4 @@ from selenium import webdriver
 driver = webdriver.Firefox()
 driver.get("http://localhost:8000")
 assert 'Django' in driver.title
+
