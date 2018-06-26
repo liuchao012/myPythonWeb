2d10ea960f5a0c147dd34b75bfb03ff7f2f6f5b6
liuch <liuchao012@126.com>
2018-06-26 17:38:53 +0800

    -

--- functional_tests.py
+++ functional_tests.py
@@ -2,11 +2,27 @@
 # @Time    : 2018/6/25 20:15
 # @Author  : Mat
 # @Email   : mat_wu@163.com
+# @File    : functional_tests1.py
 # @Software: PyCharm
 
 from selenium import webdriver
+import unittest
 
-driver = webdriver.Firefox()
-driver.get("http://localhost:8000")
-assert 'Django' in driver.title
 
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(3)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.driver.get("http://localhost:8000")
+        self.assertIn('To-Do', self.driver.title)
+        # assert 'To-Do' in driver.title, "Browser title was:" + driver.title
+        self.fail('Finisth the test')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
